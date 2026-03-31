d4a88ee47b83e79de57a0e1ebcb5f84301a50fc2
pasindu-20 <pasindukumarage28@gmail.com>
2026-03-31 14:02:26 +0530

    fix play area payment flow

--- backend/src/routes/public.routes.js
+++ backend/src/routes/public.routes.js
@@ -4,5 +4,6 @@ const controller = require("../controllers/public.controller");
 router.get("/classes", controller.listPublicClasses);
 router.get("/events", controller.listPublicEvents);
 router.get("/play-areas", controller.listPublicPlayAreas);
+router.get("/play-areas/availability", controller.getPlayAreaAvailability);
 
 module.exports = router;
